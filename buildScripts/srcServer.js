ab02ad1dd682fca4ab32dc55440b89b20427d0da
Marco De Jesus <marcodejesus2010@gmail.com>
2018-04-14 04:41:29 -0500

    Updating the code for replacing function by =>

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -5,7 +5,7 @@ var open = require('open');
 var port = 3000
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
